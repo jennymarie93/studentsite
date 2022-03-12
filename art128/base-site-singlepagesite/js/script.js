b81c5015745f2886cce1014bc58b2608affdfe15
Jenny Zuniga <jmzuniga@hawaii.edu>
2022-03-12 07:49:24 -1000

    Revised Base-Content

---
+++ art128/base-site-singlepagesite/js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // TOGGLE NAV GLOBAL MENU FOR SMALL SCREENS
+    const menubutton = document.querySelector('.menu-button');
+    const menunav = document.querySelector('.toggle-nav');
+    menubutton.addEventListener('click', function() {
+        // console.log("hello!");
+        if (menunav.getAttribute('data-navstate') === 'open') {
+            // if true do this:
+            menunav.setAttribute('data-navstate', 'closed');
+        } else {
+            // else (if false) do this:
+            menunav.setAttribute('data-navstate', 'open');
+        };
+    });
+
+// SMOOTH SCROLL
+
+
+
+});
