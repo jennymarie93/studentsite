41ce42b65a8f0862c22dea3e308614d5ae4218b5
Jenny Zuniga <jmzuniga@hawaii.edu>
2022-04-21 12:05:21 -1000

    updated animations css

--- art128/arbor-global-draft3/js/script.js
+++ art128/arbor-global-draft3/js/script.js
@@ -24,3 +24,12 @@ document.addEventListener('DOMContentLoaded', function() {
         };
     };
 });
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
